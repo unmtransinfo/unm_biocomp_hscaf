befd175da409b27374d005f6431a2df94e361d02
Jeremy Yang <jeremyjyang@gmail.com>
2023-11-01 10:16:16 -0600

    Alright.

--- unm_biocomp_hscaf/src/test/java/edu/unm/health/biocomp/hscaf/AppTest.java
+++ unm_biocomp_hscaf/src/test/java/edu/unm/health/biocomp/hscaf/AppTest.java
@@ -1,8 +1,17 @@
 package edu.unm.health.biocomp.hscaf;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import junit.framework.*; //Test, TestCase, TestSuite
+
+import chemaxon.formats.*; //MolFormatException
+import chemaxon.marvin.io.MolExportException;
+import chemaxon.sss.search.SearchException;
+import chemaxon.struc.Molecule;
+
+import edu.unm.health.biocomp.hscaf.*; //ScaffoldSet, ScaffoldTree, ScaffoldException
 
 /**
  * Unit test for simple App.
@@ -34,5 +43,22 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    /**
+     * Better Test :-)
+     */
+    public void testScaf()
+	throws Exception
+	//throws IOException, SQLException, MolFormatException, MolExportException, SearchException, ScaffoldException
+    {
+      String smi = "COc1cc2c(ccnc2cc1)C(O)C4CC(CC3)C(C=C)CN34"; //quinine
+      Molecule mol = MolImporter.importMol(smi, "smiles:");
+      ScaffoldSet scafset = new ScaffoldSet("test");
+
+      boolean stereo = false;
+      boolean keep_nitro_attachments = false;
+      ScaffoldTree scaftree = new ScaffoldTree(mol, stereo, keep_nitro_attachments, scafset);
+      assertTrue( scaftree.getScaffoldCount() > 0 );
     }
 }
